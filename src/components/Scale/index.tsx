22097d617922d40f93a50da98d9dbe558a405cee
TailssTF <tailss5@mail.ru>
2025-07-16 12:44:54 +0700

    initial commit

---
+++ src/components/Scale/index.tsx
@@ -0,0 +1,10 @@
+export const Scale = () => {
+  return (
+    <div className="scale">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia
+      facilis temporibus ad beatae inventore suscipit provident expedita est
+      voluptatum adipisci laborum animi vel saepe, rem eaque doloremque officiis
+      odio.
+    </div>
+  );
+};
