bc70009952816bba751d3780d994a4bc273c8412
TailssTF <tailss5@mail.ru>
2025-07-17 10:45:47 +0700

    feat: добавил content

--- src/components/Scale/index.tsx
+++ src/components/Scale/index.tsx
@@ -1,10 +1,14 @@
 export const Scale = () => {
   return (
-    <div className="scale">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia
-      facilis temporibus ad beatae inventore suscipit provident expedita est
-      voluptatum adipisci laborum animi vel saepe, rem eaque doloremque officiis
-      odio.
+    <div className="scale_wrap">
+      <div className="scale_body">
+        <p className="scale_text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia
+          facilis temporibus ad beatae inventore suscipit provident expedita est
+          voluptatum adipisci laborum animi vel saepe, rem eaque doloremque
+          officiis odio.
+        </p>
+      </div>
     </div>
   );
 };
