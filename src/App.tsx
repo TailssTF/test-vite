8a8e41c9fadad06ac257be9f27272b4fd151f0ba
TailssTF <tailss5@mail.ru>
2025-07-18 18:18:38 +0700

    feat: добавил footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
+import "@coreui/coreui/dist/css/coreui.min.css";
+import "./styles/index.scss";
+
 import { Header } from "./components/Header";
 import { Progress } from "./components/Progress";
 import { Scale } from "./components/Scale";
-import "@coreui/coreui/dist/css/coreui.min.css";
-import "./styles/index.scss";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="wrap">
-        <Header />
-        <Progress />
-        <div className="content">
-          {new Array(20).fill(0).map((_, index) => (
-            <Scale key={index} />
-          ))}
+        <div className="main">
+          <Header />
+          <Progress />
+          <div className="content">
+            {new Array(10).fill(0).map((_, index) => (
+              <Scale key={index} />
+            ))}
+          </div>
         </div>
+        <Footer />
       </div>
     </>
   );
