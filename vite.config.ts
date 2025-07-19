19c8ef432ccba4318bd1692afc009ed314a1bb30
TailssTF <tailss5@mail.ru>
2025-07-19 13:05:02 +0700

    fix(vite-config): сменил base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/test-vite/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
